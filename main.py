bf349623c013d76d10d9028272ef635a3d508e21
haddelalshahabi <s362080@oslomet.no>
2023-09-22 10:09:48 +0200

    Added some changes to these files

--- main.py
+++ main.py
@@ -1,54 +1,31 @@
-"""from kivy.app import App
-from ui_components import MainLayout
-from gallery_access import open_filechooser
-from audio_feedback import speak_text
-from object_detection import process_image
-from voice_commands import get_voice_command
+"""
+from kivy.app import App
+from main_layout import MainLayout  # Import the MainLayout you created
+from voice_commands import capture_voice_command, interpret_command  # Import the voice command functions
 
 class MainApp(App):
+
     def build(self):
         return MainLayout()
+
+    def handle_voice_command(self):
+        captured_command = capture_voice_command()
+        action = interpret_command(captured_command)
+
+        if action == "open_gallery":
+            # Code to open the gallery
+            self.root.open_gallery(None)  # Assuming your open_gallery method in MainLayout doesn't require the 'instance' argument
+        elif action == "describe_image":
+            # Code to describe the currently displayed image
+            pass
+        elif action == "show_help":
+            # Code to show help instructions
+            pass
+        # ... additional actions based on other voice commands
 
 if __name__ == "__main__":
-    MainApp().run()
-"""
-""""
-from kivy.app import App
-from kivy.logger import Logger
-from ui_components import MainLayout
-from gallery_access import open_filechooser
-from audio_feedback import speak_text
-from object_detection import process_image
-from voice_commands import get_voice_command
-
-class MainApp(App):
-
-    def build(self):
-        return MainLayout()
-
-    def on_start(self):
-        Kommentar 
-        This method is called when the app starts.
-        Use this to provide any initial feedback or setup.
-       
-        speak_text("Welcome to the Image Accessibility App. Please select an image or use voice commands.")
-
-    def handle_image_selection(self, *args):
-        
-        Kommentar
-        Handle the event when an image is selected.
-       
-        try:
-            image_path = open_filechooser()
-            detected_objects = process_image(image_path)
-            feedback_message = f"Detected objects are: {', '.join(detected_objects)}"
-            speak_text(feedback_message)
-        except Exception as e:
-            Logger.error(f"Error during image selection: {e}")
-            speak_text("Sorry, an error occurred while processing the image.")
-
-    # Add more handlers for other events or functionalities as needed.
-
+    app = MainApp()
+    app.run()
 
 """
 
--- voice_commands.py
+++ voice_commands.py
@@ -6,3 +6,43 @@ def get_voice_command():
         print("Say something:")
         audio = recognizer.listen(source)
     return recognizer.recognize_google(audio)
+
+"""
+import speech_recognition as sr
+
+def capture_voice_command():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source)
+        print("Listening for a command:")
+        audio = recognizer.listen(source)
+
+        try:
+            command_text = recognizer.recognize_google(audio)
+            print(f"Recognized command: {command_text}")
+            return command_text.lower()  # Converting to lower case for easy comparison
+
+        except sr.UnknownValueError:
+            print("Could not understand the audio.")
+            return None
+        except sr.RequestError as e:
+            print(f"Could not request results; {e}")
+            return None
+
+
+def interpret_command(command):
+    if command:
+        if "open gallery" in command:
+            return "open_gallery"
+        elif "describe image" in command:
+            return "describe_image"
+        elif "help" in command:
+            return "show_help"
+        # Add more elif conditions to handle additional voice commands
+        else:
+            print("Command not recognized.")
+            return None
+    else:
+        return None
+
+"""
